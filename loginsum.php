f9f8705a9b7ff2733b142065be8cc8ad95c50438
a1133348 <a1133348@mail.nuk.edu.tw>
2026-04-09 09:36:06 +0800

    Create loginsum.php

---
+++ loginsum.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>登入頁面</title>
+</head>
+<body bgcolor=papayawhip>
+<center>
+<B><I><font size=80 color=black>系統登入</font></I></B>
+<hr><br>
+<form action="loginchecksum.php" method="POST">
+ID:<input type="text" name="uID"><br>
+<br>
+Password:<input type="password" name="uPWD"><br>
+<br>
+<input type="submit" value="登入">
+</form>
+</center>
+</body>
+</html>
